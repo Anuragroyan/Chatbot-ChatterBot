ea4c05c27f67073fddea143f9f81b035c4c1b19c
Anuragroy1 <anuragroy.xworkx@gmail.com>
2022-06-09 00:45:05 +0530

    chatbot app with complete code

---
+++ Rule-Based-Chatbot/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+app = Flask(__name__)
+
+chatbot = ChatBot('Dungeon_master')
+
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+trainer.train("chatterbot.corpus.english.greetings",
+              "chatterbot.corpus.english.conversations",
+              "./Data/conversation.yml")
+
+@app.route("/")
+
+def index():
+    return render_template("index.html")
+
+
+@app.route("/get", methods=["GET","POST"])
+
+def chatbot_response():
+    msg = request.form["msg"]
+    response = chatbot.get_response(msg)
+    return str(response)
+
+
+if __name__ == "__main__":
+
+    app.run()             
+
